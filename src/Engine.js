c7d5f5437f0296ba83d8073cef775ef599f2442f
massawi33 <zakaria.hza@gmail.com>
2015-11-26 16:52:17 +0100

    Histoire 7 & 8

--- src/Engine.js
+++ src/Engine.js
@@ -5,16 +5,15 @@ var Engine = function () {
 // private attributes and methods
     //var plateau = new Array(6);
 
-    var player_1 = new Array(6);// 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
-    var player_2 = new Array(6);// 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
-    var plateau = [["noir", "vert", "blanc", "bleu", "rouge", "blanc"],
+    var player_1 = new Array(6), player_2 = new Array(6), i, j, last_player, plateau, winner;// 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
+    // 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
+    plateau = [["noir", "vert", "blanc", "bleu", "rouge", "blanc"],
         ["jaune", "blanc", "vert", "rouge", "jaune", "bleu"],
         ["bleu", "jaune", "bleu", "blanc", "noir", "rouge"],
         ["rouge", "noir", "rouge", "vert", "bleu", "blanc"],
         ["blanc", "vert", "jaune", "noir", "jaune", "vert"],
-        ["jaune", "bleu", "noir", "rouge", "vert", "noir"]
-    ];
-    var i,j;
+        ["jaune", "bleu", "noir", "rouge", "vert", "noir"]];
+
     /*this.init = function() {
         for (i = 0; i < 6; i++) {
 
@@ -28,14 +27,14 @@ var Engine = function () {
         }
 
     };*/
-    this.juxtaposition = function(){
+    this.juxtaposition = function () {
 
         // test horisontal
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < 5; i = i + 1) {
 
 
 
-            for (j = 0; j < 6; j++) {
+            for (j = 0; j < 6; j = j + 1) {
 
                 if (plateau[i][j] === plateau[i + 1][j] && plateau[i][j] !== "empty") {
 
@@ -46,11 +45,11 @@ var Engine = function () {
 
             }
         }
-        for (i = 0; i < 6; i++) {
+        for (i = 0; i < 6; i = i + 1) {
 
 
             // test Vertical
-            for (j = 0; j < 5; j++) {
+            for (j = 0; j < 5; j = j + 1) {
 
                 if (plateau[j][i] === plateau[j + 1][i] && plateau[j][i] !== "empty" ) {
 
@@ -63,8 +62,89 @@ var Engine = function () {
         }
 
     };
-    this.check_voisin = function(a ,b){
-            var check = 0 ;
+    this.check_left = function (a, b) {
+        if (plateau[a][b + 1] !== "empty") {
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    this.check_right = function (a, b) {
+        if(plateau[a][b - 1] !== "empty" ){
+
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+    this.check_botom = function(a, b){
+
+        if(plateau[a - 1][b] !== "empty" ){
+
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+
+    this.check_top = function(a, b){
+
+        if(plateau[a + 1][b] !== "empty" ){
+
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+
+    this.check_Connection = function (a, b) {
+
+
+        if ((plateau[a][b + 1] === "empty" && plateau[a][b - 1] === "empty") && ( plateau[a + 1][b] === "empty" && plateau[a + 1][b] === "empty")) {
+
+            return false;
+
+
+        } else if (plateau[a + 1][b] === "empty" && plateau[a + 1][b] === "empty" ) {
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+
+
+    };
+    this.check_win_plateau = function(couleur) {
+
+        for (i = 0; i < 5; i = i + 1) {
+
+
+
+            for (j = 0; j < 6; j = j + 1) {
+
+                if (plateau[i][j] === couleur ) {
+
+                   return false;
+
+
+                }
+
+            }
+        }
+        return true;
+
+
+    };
+    this.check_voisin = function (a, b) {
+
+        var check = 0;
         if (a === 5 && b === 5) {
 
             if (plateau[a][b - 1] !== "empty") {
@@ -82,10 +162,10 @@ var Engine = function () {
 
                 return false;
 
-            } else {
-
+            }else {
                 return true;
             }
+
         }else if (a === 0 && b === 0) {
             if (plateau[a][b + 1] !== "empty") {
                 check = check + 1;
@@ -191,7 +271,7 @@ var Engine = function () {
 
                 return true;
             }
-        }else if (b === 5) {
+        } else if (b === 5) {
 
             if (plateau[a][b - 1] !== "empty") {
 
@@ -215,7 +295,7 @@ var Engine = function () {
 
                 return true;
             }
-        }else if (b === 0) {
+        } else if (b === 0) {
             if (plateau[a][b + 1] !== "empty") {
                 check = check + 1;
             }
@@ -258,9 +338,7 @@ var Engine = function () {
             }
 
             if (check > 2) {
-
                 return false;
-
             } else {
 
                 return true;
@@ -270,35 +348,35 @@ var Engine = function () {
 
 
     };
-    this.check_player_1 = function(){
+    this.check_player_1 = function () {
 
         return player_1;
 
     };
-    this.check_player_2 = function(){
+    this.check_player_2 = function () {
 
         return player_2;
 
     };
-    this.move_player1 =function(a , b , color){
+    this.move_player1 = function (a, b, color) {
         //var found = -1;
-        if(plateau[a][b] === color){
+        if (plateau[a][b] === color) {
 
             plateau[a][b] = "empty";
             this.juxtaposition();
             // 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
             if (color === "jaune") {
-                player_1 [0] = player_1 [0] + 1;
-            }else if (color === "noir") {
-                player_1 [1] = player_1 [1] + 1;
-            }else if (color === "rouge") {
-                player_1 [2] = player_1 [2] + 1;
-            }else if (color === "bleu") {
-                player_1 [3] = player_1 [3] + 1;
-            }else if (color === "blanc") {
-                player_1 [4] = player_1 [4] + 1;
-            }else if (color === "vert") {
-                player_1 [5] = player_1 [5] + 1;
+                player_1[0] = player_1[0] + 1;
+            } else if (color === "noir") {
+                player_1[1] = player_1[1] + 1;
+            } else if (color === "rouge") {
+                player_1[2] = player_1[2] + 1;
+            } else if (color === "bleu") {
+                player_1[3] = player_1[3] + 1;
+            } else if (color === "blanc") {
+                player_1[4] = player_1[4] + 1;
+            } else if (color === "vert") {
+                player_1[5] = player_1[5] + 1;
             }
 
             return true;
@@ -307,13 +385,17 @@ var Engine = function () {
             return false;
         }
 
+        last_player = "1";
+        if (this.check_win_plateau(color)){
 
+            winner = "1";
+        }
 
 
 
 
     };
-    this.move_player2 =function(a , b , color){
+    this.move_player2 = function (a, b, color) {
         //var found = -1;
         if(plateau[a][b] === color){
 
@@ -321,39 +403,36 @@ var Engine = function () {
             this.juxtaposition();
             // 0 jaune , 1 noir , 2 rouge , 3 bleu , 4 blanc , 5 vert
             if (color === "jaune") {
-                player_2 [0] = player_2 [0] + 1;
-            }else if (color === "noir") {
-                player_2 [1] = player_2 [1] + 1;
-            }else if (color === "rouge") {
-                player_2 [2] = player_2 [2] + 1;
-            }else if (color === "bleu") {
-                player_2 [3] = player_2 [3] + 1;
-            }else if (color === "blanc") {
-                player_2 [4] = player_2 [4] + 1;
-            }else if (color === "vert") {
-                player_2 [5] = player_2 [5] + 1;
+                player_2[0] = player_2[0] + 1;
+            } else if (color === "noir") {
+                player_2[1] = player_2[1] + 1;
+            } else if (color === "rouge") {
+                player_2[2] = player_2[2] + 1;
+            } else if (color === "bleu") {
+                player_2[3] = player_2[3] + 1;
+            } else if (color === "blanc") {
+                player_2[4] = player_2[4] + 1;
+            } else if (color === "vert") {
+                player_2[5] = player_2[5] + 1;
             }
 
-                return true;
+            return true;
 
-        }else {
+        } else {
             return false;
+        }
+        last_player = "2";
+        if (this.check_win_plateau(color)){
+
+            winner = "2";
         }
 
 
 
 
 
-
     };
-    this.check_Connection = function(a,b){
 
-
-
-
-
-
-    }
     /*this.firstmove_player2= function(){
 
         var found = -1;
--- test/test.js
+++ test/test.js
@@ -17,15 +17,15 @@ PalettoTestCase.prototype.testStory2 = function () {
 };
 PalettoTestCase.prototype.testStory3 = function () {
 
-    assertTrue(a.check_player_1()[0] === 1 );
+    assertTrue(a.check_player_1()[0] === 1);
 
 };
 PalettoTestCase.prototype.testStory4 = function () {
 
-    assertTrue(a.check_voisin(0,0));
-    assertTrue(a.move_player2(0,0,"noir"));
-    assertTrue(a.check_voisin(5,5));
-    assertTrue(a.move_player2(5,5,"noir"));
+    assertTrue(a.check_voisin(0, 0));
+    assertTrue(a.move_player2(0, 0, "noir"));
+    assertTrue(a.check_voisin(5, 5));
+    assertTrue(a.move_player2(5, 5, "noir"));
 
     //a.display_care();
 
@@ -34,13 +34,24 @@ PalettoTestCase.prototype.testStory5 = function () {
 
 
 
-    assertTrue(a.check_Connection(2,2));
+   // assertTrue(a.check_Connection(2,2));
 
 
 
 
+};
 
-    assertTrue(a.check_voisin(0,0));
+PalettoTestCase.prototype.testStory6and7 = function () {
+
+
+
+    // assertTrue(a.check_Connection(2,2));
+
+
+
+    // la fonction check_win doit etre après après chaque coups , elle verifie la couleur jouer par le joueur , si il ne reste plus de coupleur le dernier joueur à gagner
+
+
 
 
 };
